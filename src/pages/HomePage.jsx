3de2b9aacbeb5b3565cac7fb2f453efa3ab1f4ff
zhenkun.xie <963157561@qq.com>
2023-10-14 19:55:47 +0800

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom"
 import Styled from "styled-components";
+import { Button, DatePicker, Form, Input } from 'antd';
 import ReactFullpage from "@fullpage/react-fullpage";
 
 import logoImg from "../assets/images/logo.png";
@@ -8,11 +9,31 @@ import img1Img from "../assets/images/img1.png";
 import img2Img from "../assets/images/img2.png";
 import img3Img from "../assets/images/img3.png";
 import bannerBgImg from "../assets/images/banner-bg.png";
+import bannerBg1Img from "../assets/images/banner-bg1.png";
+import imgaImg from "../assets/images/imga.png";
+import imgbImg from "../assets/images/imgb.png";
+import imgcImg from "../assets/images/imgc.png";
+import imgdImg from "../assets/images/imgd.png";
+import imgeImg from "../assets/images/imge.png";
+import imgfImg from "../assets/images/imgf.png";
+import icon1Img from "../assets/images/icon1.png";
+import icon2Img from "../assets/images/icon2.png";
+import icon3Img from "../assets/images/icon3.png";
+
+
 
 import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
   const [menuActive, setMenuActive] = useState(0) 
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
   return (
     <StyledWapper>
@@ -51,56 +72,90 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
-            <div className="section s2">
-              <div className="left-wrap">
-                <div className="content">
-                  <div className="title">กรอกข้อมูลเพื่อให้ผู้เชี่ยวชาญดูแล</div>
-                  <div className="form-wrap">
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
+            <div className="section s21">
+              <div className="w-full bg-[#22CF68] flex flex-col items-center  justify-center px-[10vw]">
+                <div className="flex flex-col items-center w-full max-w-[1310px] py-[58px]">
+                  <div className="mb-[60px] text-center text-white text-[36px]">ปรึกษา ออกแบบ ติดตั้งโซล่าร์เซล์ ฟรี!</div>
+                  <div className="flex justify-between items-center w-full">
+                    <div className="inline-flex items-center">
+                      <img className="inline-block w-[70px] h-[70px] mr-[20px] object-fill" src={icon1Img} alt="" />
+                      <div className="inline-flex flex-col">
+                        <div className="text-[22px] text-white">สถานที่</div>
+                        <div className="mt-[12px] w-[250px] text-[14px] text-white">8th floor - Palace Building - 221b Baker street - London - UK</div>
+                      </div>
                     </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
+                    <div className="inline-flex items-center">
+                      <img className="inline-block w-[70px] h-[70px] mr-[20px] object-fill" src={icon2Img} alt="" />
+                      <div className="inline-flex flex-col">
+                        <div className="text-[22px] text-white">โทรหาเรา  </div>
+                        <div className="mt-[12px] w-[250px] text-[14px] text-white">15263654789  12547889662</div>
+                      </div>
                     </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
+                    <div className="inline-flex items-center">
+                      <img className="inline-block w-[70px] h-[70px] mr-[20px] object-fill" src={icon3Img} alt="" />
+                      <div className="inline-flex flex-col">
+                        <div className="text-[22px] text-white">กล่องจดหมาย</div>
+                        <div className="mt-[12px] w-[250px] text-[14px] text-white">12545555555@139.com</div>
+                      </div>
                     </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div>
-                    <div className="form-item">
-                      <div className="form-label">* First Name</div>
-                      <div className="form-content"><input type="text" /></div>
-                    </div> 
                   </div>
                 </div>
               </div>
-              <div className="right-wrap">
-
+              <div className="w-full bg-[#ffCcee] flex flex-col items-center  justify-center px-[10vw]">
+                <div className="flex flex-col items-center w-full max-w-[1310px] py-[58px]">
+                  <div className="mb-[60px] text-center text-[#222222] text-[36px]">กรอกข้อมูลเพื่อให้ผู้เชี่ยวชาญดูแล</div>
+                    <Form
+                    className="w-full"
+                      name="basic"
+                      layout="vertical"
+                      initialValues={{ remember: true }}
+                      onFinish={onFinish}
+                      onFinishFailed={onFinishFailed}
+                      autoComplete="off"
+                    >
+                      <div className="grid grid-cols-3 gap-x-[50px] w-full">
+                        <Form.Item label="First Name" name="name" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="นามสกุล" />
+                        </Form.Item>
+                        <Form.Item label="* Phone" name="phone" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="เบอร์ติดต่อ" />
+                        </Form.Item>
+                        <Form.Item label="* Line / Email" name="email" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="ไลน์/อีเมล" />
+                          <Input />
+                        </Form.Item>
+                        <Form.Item label="* ประเภท" name="aa" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="ประเภท" />
+                        </Form.Item>
+                        <Form.Item label="Company" name="company" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="ชื่อบริษัท" />
+                        </Form.Item>
+                        <Form.Item label="Position" name="position" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="สถานที่ตั้ง" />
+                        </Form.Item>
+                        <Form.Item label="* GPS/Address" name="address" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="พิกัด, ที่อยู่" />
+                        </Form.Item>
+                        <Form.Item label="* Elec bill/ month" name="month" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="บิลค่าไฟฟ้า/เดือน" />
+                        </Form.Item>
+                        <Form.Item label="Kick off date" name="date" rules={[{ required: true, message: 'Please input!' }]}>
+                          <DatePicker placeholder="วันที่เริ่มก่อสร้าง" />
+                        </Form.Item>
+                        <Form.Item label="* Working hour(วันทำงาน)" name="workHour" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input placeholder="เวลาทำงาน  " />
+                        </Form.Item>
+                        <Form.Item className="col-span-2" label="ข้อมูลเพื่อให้เราตอบโจทย์คุณได้มากขึ้น" name="desc" rules={[{ required: true, message: 'Please input!' }]}>
+                          <Input.TextArea autoSize={{ minRows: 3, maxRows: 5 }} placeholder="คำแนะนำอื่น ๆ เช่นข้อ จำกัด ของรายการที่ต้องการพื้นที่การลงทุนและรูปแบบระบบ" />
+                        </Form.Item>
+                        <Form.Item className="col-span-3">
+                          <div className="flex justify-center">
+                            <Button className="big-btn" type="primary" htmlType="submit">ส่งข้อมูล</Button>
+                          </div>
+                        </Form.Item>
+                      </div>
+                    </Form>
+                </div>
               </div>
             </div>
             <div className="section s3">
@@ -175,20 +230,66 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
-            <div className="section ">
-              <div className="w-full h-screen flex flex-col items-center justify-center bg-[#F0F0F0]">
-                <div className="flex flex-col items-center w-full max-w-[1310px]">
-                  <div className="head flex items-center w-full">
-                    <div className="left">Trust  ผลงานที่ผ่านมา</div>
-                    <div className="right">Trust  ผลงานที่ผ่านมา เปลี่ยนหลังคาใหม่ พร้อมเปลี่ยน skylightลดจำนวนจากเดิม  แต่สว่างขึ้นมาก ความร้อนหลังคาลดลงกว่า40องศาจากการติดตั้งsolar เราไว้วางใจให้ Manee Power ดูแลทั้งหมด
- 
-
- Total 2MWp capacity</div>
+            <div className="section s5">
+              <div className="w-full h-screen flex flex-col items-center">
+                <div className="flex flex-col items-center w-full max-w-[1310px] pb-[100px]">
+                  <div className="head flex items-center w-full h-[250px]">
+                    <div className="left w-1/2 text-center text-[36px] text-[#222222] font-bold">Trust  ผลงานที่ผ่านมา</div>
+                    <div className="right max-w-[542px] w-1/2 text-[16px] text-[#222222]">Trust  ผลงานที่ผ่านมา เปลี่ยนหลังคาใหม่ พร้อมเปลี่ยน skylightลดจำนวนจากเดิม  แต่สว่างขึ้นมาก ความร้อนหลังคาลดลงกว่า40องศาจากการติดตั้งsolar เราไว้วางใจให้ Manee Power ดูแลทั้งหมด Total 2MWp capacity</div>
+                  </div>
+                  <div className="grid grid-cols-2 w-full">
+                    <img className="w-full object-cover" src={imgaImg} alt="" />
+                    <img className="w-full object-cover" src={imgbImg} alt="" />
+                    <img className="w-full object-cover" src={imgcImg} alt="" />
+                    <img className="w-full object-cover" src={imgdImg} alt="" />
+                    <img className="w-full object-cover" src={imgeImg} alt="" />
+                    <img className="w-full object-cover" src={imgfImg} alt="" />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="section">
+            <div className="section s6">
+              <div className="relative w-full h-screen">
+                <img className="w-full h-full object-fill" src={bannerBg1Img} alt="" />
+                <div className="flex justify-center absolute top-0 left-0 w-full h-full bg-black bg-opacity-60">
+                  <div className="flex flex-col justify-center w-full max-w-[1310px] h-full">
+                    <div className="w-full max-w-[950px] text-[69px] text-white">
+                      Manee Power
+                    </div>
+                    <div className="w-full max-w-[950px] mt-[43px] text-[24px] text-white">
+                      เราเริ่มต้นธุรกิจด้วยเครื่องตอกเสาเข็มสกรูจากประเทศเยอรมนีสำหรับภาคส่วนโซลาร์ฟาร์ม ก่อตั้งขึ้นในปี 2559 ด้วยทุนจดทะเบียน5 ล้านบาท
+                    </div>
+                    <div className="w-full max-w-[950px] text-[24px] text-white">
+                      ตอนนี้ เรากำลังมุ่งเน้นไปที่การเป็นผู้ให้บริการโซลูชั่นพลังงานแสงอาทิตย์เต็มรูปแบบ รวมถึง Solar Rooftop และ Solar Farm, grid-tied หรือระบบแบตเตอรี่ รวมถึงเทคโนโลยีการนำความร้อนกลับมาใช้ใหม่ ตั้งแต่ครัวเรือนขนาดเล็กไปจนถึงอุตสาหกรรมขนาดใหญ่ ผลักดันการใช้พลังงานสะอาดเพื่อสิ่งแวดล้อมที่ดีในสังคมและส่งต่อให้คนรุ่นต่อไป                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div className="section s7 bg-[#16243F]">
+              <div className="w-full flex flex-col items-center justify-center px-[10vw] py-[55px]">
+                <div className="flex flex-col w-full max-w-[1310px]">
+                  <div className="text-white text-[40px]">ติดต่อเรา</div>
+                  <div className="flex justify-between mt-[24px]">
+                    <div className="flex flex-col max-w-[50%] text-[#8c8f94] font-bold">
+                      <div className="title text-[24px] text-white">Opportunity-การจ้างงาน </div>
+                      <div className="flex items-center mt-[24px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[260px]">หัวหน้าบัญชี</div><div>1</div></div>
+                      <div className="flex items-center mt-[16px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[260px]">ธุรการโครงการ</div><div>1</div></div>
+                      <div className="flex items-center mt-[16px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[260px]">Business development</div><div>1</div></div>
+                      <div className="flex items-center mt-[16px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[260px]">sale manager</div><div>1</div></div>
+                      <div className="flex items-center mt-[16px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[260px]">Design Engineer</div><div>2</div></div>
+                      <div className="flex flex-col mt-[16px] text-[14px] font-normal"><div className="label">หากต้องการสมัครงานกับเรา กรุณาส่งจดหมายสมัครงานพร้อมประวัติของคุณมาที่: </div><div>achiraya.ch@maneepower.com  โทร 088-269-4245</div></div>
+                    </div>
+                    <div className="flex flex-col max-w-[50%] text-[#8c8f94] font-bold">
+                      <div className="title text-[24px] text-white">สำนักงานใหญ่</div>
+                      <div className="flex flex-col mt-[24px] text-[14px] font-normal">263 ซ.เพชรเกษม 84 แขวงบางแคเหนือ เขตบางแค กรุงเทพมหานคร 10160</div>
+                      <div className="title mt-[24px] text-[24px] text-white">ติดต่อ ฝ่ายขาย</div>
+                      <div className="flex items-center mt-[24px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[160px]">Tel:</div><div>094-942-5936 (แอม), 088-985-2324(ปิง)</div></div>
+                      <div className="flex items-start mt-[16px] text-[18px]"><div className="label flex-shrink-0 flex-grow-0 w-[160px]">Email:</div><div className="flex flex-col"><div>Chatsuda.ch@maneepower.com</div><div>Mahaphon.ch@maneepower.com</div></div></div>
+                      <div className="flex flex-col mt-[16px] text-[14px] font-normal">ติดต่อ ฝ่ายจัดซื้อ เสนออุปกรณ์ 096-995-5368</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </ReactFullpage.Wrapper>
         )}
@@ -212,12 +313,18 @@ const StyledWapper = Styled.div`
       height: calc(100vh - 88px);
     }
   }
-  .s2 {}
+  .s21,.s22 {
+    height: auto;
+  }
   .s3 {}
   .s4 {}
-  .s5 {}
+  .s5 {
+    height: auto;
+  }
   .s6 {}
-  .s7 {}
+  .s7 {
+    height: auto;
+  }
 `;
 
 export default HomePage;
